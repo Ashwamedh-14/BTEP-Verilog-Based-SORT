dfcb4583dd2d74836a9e617c3a1920ceedcc66de
Ashwamedh <113002273+Ashwamedh-14@users.noreply.github.com>
2026-03-26 17:54:48 +0530

    Made some Specs for the hardware

--- sort_verilog/arithmetic.v
+++ sort_verilog/arithmetic.v
@@ -1,37 +1,16 @@
-
-
-// half adder
-module Half_Adder(S, Cout, A, B);
-input A, B;
-output wire S, Cout;
-
-assign S = A ^ B;
-assign Cout = A & B;
-
-endmodule
-
-
-// full adder
-module Full_Adder(S, Cout, A, B, Cin);
-input A, B, Cin;
-output wire S, Cout;
-wire [1:0] temp;
-
-assign S = Cin ^ A ^ B;
-assign temp[0] = A & B;
-assign temp[1] = Cin & (A ^ B);
-assign Cout = temp[0] | temp[1];
-
-endmodule
-
 // 16 bit adder
-module Adder_16bit_Signed(S, Cout, A, B, Cin);
+module Adder_Subtractor_16bit(S, Cout, Overflow, A, B, Sub);
 input [15:0] A, B;
-input Cin;
+input Sub;
 
-output wire [15:0] S;
-output wire Cout;
+output wire Cout, Overflow;
+output wire [15:0] Sum;
 
+wire [16:0] S_temp;
+wire [15:0] B_mask;
 
-
+assign B_mask = B ^ {16{Sub}};
+assign S_temp = A + B_mask + Sub;
+assign {Cout, S} = S_temp;
+assign Overflow = (A[15] == B_mask[15]) && (S[15] != A[15]);
 endmodule
--- sort_verilog/memory.v
+++ sort_verilog/memory.v
@@ -1,14 +1,16 @@
 
 
 // 8 bit register
-module Reg_8bit(Out, In, clk, rst);
+module Reg_8bit(Out, In, clk, rst, write_en);
 input [7:0] In;
-input clk, rst;
+input clk, rst, write_en;
 output reg [7:0] Out;
 
 always @(posedge clk) begin
     if (rst == 1'b1)
         Out <= 8'b00000000;
+    else if (write_en == 1'b0)
+        Out <= Out;
     else
         Out <= In;
 end
@@ -17,39 +19,146 @@ endmodule
 
 
 // 16 bit register using 8 bit register
-module Reg_16bit(Out, In, clk, rst);
+module Reg_16bit(Out, In, clk, rst, write_en);
 input [15:0] In;
-input rst, clk;
+input rst, clk, write_en;
 
 output wire [15:0] Out;
 
-Reg_8bit inst_low(Out[7:0], In[7:0], clk, rst);
-Reg_8bit inst_high(Out[15:8], In[15:8], clk, rst);
+Reg_8bit inst_low(Out[7:0], In[7:0], clk, rst, write_en);
+Reg_8bit inst_high(Out[15:8], In[15:8], clk, rst, write_en);
 
 endmodule
 
 // 32 bit register using 16 bit registers
-module Reg_32bit(Out, In, clk, rst);
+module Reg_32bit(Out, In, clk, rst, write_en);
 input [31:0] In;
-input clk, rst;
+input clk, rst, write_en;
 
 output wire [31:0] Out;
 
-Reg_16bit inst_low(Out[15:0], In[15:0], clk, rst);
-Reg_16bit inst_high(Out[31:16], In[31:16], clk, rst);
+Reg_16bit inst_low(Out[15:0], In[15:0], clk, rst, write_en);
+Reg_16bit inst_high(Out[31:16], In[31:16], clk, rst, write_en);
 
 endmodule
 
 
 // 64 bit register using 32 bit registers
 
-module Reg_64bit(Out, In, clk, rst);
+module Reg_64bit(Out, In, clk, rst, write_en);
 input [63:0] In;
-input clk, rst;
+input clk, rst, write_en;
 
 output wire [63:0] Out;
 
-Reg_32bit inst_low(Out[31:0], In[31:0], clk, rst);
-Reg_32bit inst_high(Out[63:32], In[63:32], clk, rst);
+Reg_32bit inst_low(Out[31:0], In[31:0], clk, rst, write_en);
+Reg_32bit inst_high(Out[63:32], In[63:32], clk, rst, write_en);
+
+endmodule
+
+module X_Block(        // Block to store the State register
+    clk, rst, en, cx, cy, s, r, vx, vy, vs,
+    cx_out, cy_out, s_out, r_out, vx_out, vy_out, vs_out
+);
+
+input clk, rst, en;
+input [15:0] cx, cy, r, vx, vy, vs;
+input [19:0] s;
+
+output reg [15:0] cx_out, cy_out, r_out, vx_out, vy_out, vs_out;
+output reg [19:0] s_out;
+
+always @(posedge clk) begin
+    if (rst == 1'b1) begin
+        cx_out <= 16'b0000_0000_0000_0000;
+        cy_out <= 16'b0000_0000_0000_0000;
+        s_out <= 20'b0000_0000_0000_0000_0000;
+        r_out <= 16'b0000_0000_0000_0000; 
+        vx_out <= 16'b0000_0000_0000_0000;
+        vy_out <= 16'b0000_0000_0000_0000;
+        vs_out <= 16'b0000_0000_0000_0000;
+    end
+    else if (en == 1'b1) begin
+        cx_out <= cx;
+        cy_out <= cy;
+        s_out <= s;
+        r_out <= r;
+        vx_out <= vx;
+        vy_out <= vy;
+        vs_out <= vs;
+    end
+    else;
+end
+endmodule
+
+module P_block(     // block to hold the covariance matrix
+    clk, rst, en, x, y, s, r, vx, vy, vs, cov_vx, cov_vy, cov_vs,
+    x_out, y_out, s_out, r_out, vx_out, vy_out, vs_out, cov_vx_out, cov_vy_out, cov_vs_out
+);
+
+input clk, rst, en;
+input [15:0] x, y, s, r, vx, vy, vs, cov_vx, cov_vy, cov_vs;
+
+output reg [15:0] x_out, y_out, s_out, r_out, vx_out, vy_out, vs_out, cov_vx_out, cov_vy_out, cov_vs_out;
+
+always @(posedge clk) begin
+    if (rst == 1'b1) begin
+        x_out <= 16'b0000_0000_0000_0000;
+        y_out <= 16'b0000_0000_0000_0000;
+        s_out <= 16'b0000_0000_0000_0000;
+        r_out <= 16'b0000_0000_0000_0000;
+        vx_out <= 16'b0000_0000_0000_0000;
+        vy_out <= 16'b0000_0000_0000_0000;
+        vs_out <= 16'b0000_0000_0000_0000;
+        cov_vx_out <= 16'b0000_0000_0000_0000;
+        cov_vy_out <= 16'b0000_0000_0000_0000;
+        cov_vs_out <= 16'b0000_0000_0000_0000;
+    end
+    else if (en == 1'b1) begin
+        x_out <= x;
+        y_out <= y;
+        s_out <= s;
+        r_out <= r;
+        vx_out <= vx;
+        vy_out <= vy;
+        vs_out <= vs;
+        cov_vx_out <= cov_vx;
+        cov_vy_out <= cov_vy;
+        cov_vs_out <= vs_out;
+    end
+    else ;
+end
+endmodule
+
+module Q_block(   // block to hold noise variance 
+    clk, rst, en, x, y, s, r, vx, vy, vs,
+    x_out, y_out, s_out, r_out, vx_out, vy_out, vs_out
+);
+
+input clk, rst, en;
+input [16:0] x, y, s, r, vx, vy, vs;
+output reg [16:0] x_out, y_out, s_out, r_out, vx_out, vy_out, vs_out;
+
+always @(posedge clk) begin
+    if (rst == 1'b1) begin
+        x_out <= 16'b0000_0000_0000_0000;
+        y_out <= 16'b0000_0000_0000_0000;
+        s_out <= 16'b0000_0000_0000_0000;
+        r_out <= 16'b0000_0000_0000_0000;
+        vx_out <= 16'b0000_0000_0000_0000;
+        vy_out <= 16'b0000_0000_0000_0000;
+        vs_out <= 16'b0000_0000_0000_0000;
+    end
+    else if (en == 1'b1) begin
+        x_out <= x;
+        y_out <= y;
+        s_out <= s;
+        r_out <= r;
+        vx_out <= vx;
+        vy_out <= vy;
+        vs_out <= vs;
+    end
+    else ;
+end
 
 endmodule
